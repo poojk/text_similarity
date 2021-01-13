2cabd66bce10bed754263f3029b875fd49733331
Raveena Pooja Kankeyan <66836884+poojk@users.noreply.github.com>
2021-01-13 13:24:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,6 @@ def crux(a,b):
 
 #if __name__ == '__main__':
 #    app.run_server(debug=True)
-    
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
